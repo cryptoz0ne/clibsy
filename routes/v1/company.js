5321bd0c60920cc215bac2ebaca8a7aea628ad44
Joe <joe@clibsy.com>
2016-01-25 14:17:22 -0800

    Added initial company API documentation

---
+++ routes/v1/company.js
@@ -0,0 +1,21 @@
+//******************************************************************************
+// Copyright (c) 2015-2016 Clibsy, LLC -- All rights reserved
+//
+// Unauthorized copying of this file, via any medium, is strictly prohibited.
+// Copying or distributing requires the expressed permission of Clibsy, LLC.
+//
+// PROJECT: clibsy.com
+//
+// FILE: company.js
+//
+// DESCRIPTION: Definition of the company API endpoints.
+//
+// AUTHOR: Joe Kramer joe@clibsy.com 2015/01/12
+//******************************************************************************
+'use strict';
+
+/**
+ * @apiDefine Company Company
+ *
+ * API endpoints that interact with company resources.
+ */
